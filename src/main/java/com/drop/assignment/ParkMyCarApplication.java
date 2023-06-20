1c5ae1f5c3cb04de165db42f89b2c6a5321e5cc8
Bijaya Bhaskar Swain <Bijaya.BhaskarSwain@navihealth.com>
2023-06-20 16:53:16 +0530

    initial commit

---
+++ src/main/java/com/drop/assignment/ParkMyCarApplication.java
@@ -0,0 +1,12 @@
+package com.drop.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkMyCarApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ParkMyCarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drop/assignment/ParkMyCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drop.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkMyCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
